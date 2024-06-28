3f44d4cb17f0aaf25e4f474f0367efa27060f77e
FraBer95 <berloco.francesco95@gmail.com>
2024-06-28 16:18:42 +0200

    + fix python pre-processing

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from pre_processing.statistical_analysis import statistical_test
 
 from pre_processing.features_correlation import pearson_corr
-from classification.classification import training_models
+
 from pre_processing.plot_roc_curves import plot_roc_test
 
 
@@ -18,7 +18,7 @@ if __name__=='__main__':
     loocv = True
     split_wrt_variable = "KRAS_TP53"
     label_list_full=["status", "tumor_grade_reclass"]
-    target_list=["status"]
+    target_list=["status"] #for multiple training at once
 
     radiomics_path = '../data/radiomics_data.csv' #put your radiomics data in data folder
     clinical_path = '../data/clinical_data.csv' #put your clinical data in data folder
@@ -36,51 +36,18 @@ if __name__=='__main__':
         else: path_with_label = os.path.join(path_with_label, "4stages")
         os.makedirs(path_with_label, exist_ok=True)
 
-        #stratified train test split
-
-        features = df_origin.drop(columns=[split_wrt_variable], axis=1)
-
-        X_train, X_test, y_train, y_test = train_test_split(features, df_origin[split_wrt_variable], test_size=0.20, stratify=df_origin[label], random_state=42)
-
-
-
-        df_train = pd.concat([X_train, y_train], axis=1)
-        df_test = pd.concat([X_test, y_test], axis=1)
-
         # for el in label_list_full:
         #     print("Values distribution for Training {}: {}".format(el, df_train[el].value_counts()))
         #     print("Values distribution for Test {}: {}".format(el, df_test[el].value_counts()))
 
-        ids_train = df_train['patient_id']
-        ids_test = df_test['patient_id']
-        # df_train.to_csv(os.path.join(path_with_label, 'train.csv'), index=False)
-        # df_test.to_csv(os.path.join(path_with_label, 'test.csv'), index=False)
-        #values = df_test[label].value_counts()
-        df_train_full = add_duplicates(df_with_dup, ids_train)
-        y_train_full = df_train_full[label]
-
-        if loocv:  #a patient can have more series, so they must be in the same split
-            df_test = add_duplicates(df_with_dup, ids_test)
-            y_test_full = df_test[label]
-            df_train_full = pd.concat([df_train_full, df_test], axis=0)
-            y_train_full = pd.concat([y_train_full, y_test_full], axis=0)
-
         other_target_labels = [item for item in label_list_full if item != label]
 
-        y_train_full = df_origin[label]
+
         df_train_full = df_origin.drop(columns=other_target_labels)
 
 
         #statistical_test(df_train_full, label, path_with_label)
 
-        feature_AUC = None
-        training_features = pearson_corr(df_train_full, feature_AUC, path_with_label, IV_stages)
+        training_features = pearson_corr(df_train_full, path_with_label, IV_stages)
         training_features.append("patient_id")
         training_features.append("status")
-
-
-        classifiers_list = training_models(df_train_full[training_features], y_train_full, loocv, path_with_label,
-                                               radiomic_training=True)
-
-        if not loocv:
-            plot_roc_test(classifiers_list, df_test, training_features, label)
